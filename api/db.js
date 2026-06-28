21d049d93723b102fa78d28b382b0b111731af00
RED Arena PVP <vishwaking42@gmail.com>
2026-06-28 10:16:47 +0530

    update

--- api/db.js
+++ api/db.js
@@ -6,8 +6,10 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
+
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 export default pool;
