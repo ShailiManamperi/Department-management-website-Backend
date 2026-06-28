b979b94fdca1d46beff500630e23db9774b71f7a
RED Arena PVP <vishwaking42@gmail.com>
2026-06-28 10:47:06 +0530

    test

--- api/quotation.js
+++ api/quotation.js
@@ -1,4 +1,21 @@
-import { db } from "./db.js";
+import pool from "./db.js";
+
+export default async function handler(req, res) {
+  try {
+    const [rows] = await pool.query("SELECT 1 + 2 AS result");
+
+    res.status(200).json({
+      success: true,
+      message: "Backend connected to MySQL",
+      data: rows,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err.message,
+    });
+  }
+}
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
