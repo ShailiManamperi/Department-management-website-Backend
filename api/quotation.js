5b5b035de4feeb15ad6db97a47fa399a894d3382
RED Arena PVP <vishwaking42@gmail.com>
2026-06-28 17:14:28 +0530

    update

--- api/quotation.js
+++ api/quotation.js
@@ -1,7 +1,6 @@
 import pool from "./db.js";
 
-// const allowedOrigin = "https://deparmentmanagementwebsite.shailimanamperi2002.workers.dev";
-const allowedOrigin = "http://127.0.0.1:5502/";
+const allowedOrigin = "https://deparmentmanagementwebsite.shailimanamperi2002.workers.dev";
 
 function setCors(res) {
   res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
